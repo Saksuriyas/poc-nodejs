31cd01a13c154ecb3d63e55abe39e3bc7c918193
SKSyo <saksuriya.iwaxa@gmail.com>
2024-04-08 03:15:25 +0700

    Update port of start NodeJS Express

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require("./app");
-const PORT = process.PORT || 9080;
+const PORT = process.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log("server listen on port " + PORT);
